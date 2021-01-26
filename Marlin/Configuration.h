d1b3c26657f75ee1ec8788e033f8162e0f0d5719
Glenn-Kerselaers <Glenn-Kerselaers@users.noreply.github.com>
2021-01-27 00:25:03 +0100

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "GK 2021-01-10"
+#define CUSTOM_MACHINE_NAME "GK 2021-01-27 0013"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
